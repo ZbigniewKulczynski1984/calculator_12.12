2ce10350789a040fedf3f90b108d45bcddf78b24
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-12-13 07:09:03 +0100

    code added

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const numbers = document.querySelectorAll('.number')
-const operators = document.querySelectorAll('.operator')
-const clear = document.querySelector('.clear')
-const remove = document.querySelector('.delete')
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const clear = document.querySelector('.clear');
+const remove = document.querySelector('.delete');
 const equality = document.querySelector('.equality');
-const previousResult = document.querySelector('.previous-operation')
-const currentResult = document.querySelector('.current-operation')
+const previousResult = document.querySelector('.previous-operation');
+const currentResult = document.querySelector('.current-operation');
 
-const calculator = new Calculator(previousResult, currentResult)
+const calculator = new Calculator(previousResult, currentResult);
+
+numbers.forEach((number) => {
+	number.addEventListener('click', () => {
+		calculator.addNumber(number.innerText);
+		calculator.updateResult();
+	});
+});
+
+operators.forEach((operator) => {
+	operator.addEventListener('click', () => {
+		calculator.chooseOperation(operator.innerText);
+		calculator.updateResult();
+	});
+});
